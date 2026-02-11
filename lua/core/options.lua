88c9ab2da24768fd86b84975dddc0af4d562f369
moxkxs <pby5ws@virginia.edu>
2026-02-11 02:43:03 -0500

    Initial neovim config

---
+++ lua/core/options.lua
@@ -0,0 +1,15 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamedplus"
